8605239038118cf19734bce2b50d00d83f48f58a
Mikhail Sklyarov <mikhail.29.06.2001@gmail.com>
2023-12-07 22:59:02 +0300

    минимально рабочая версия

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ILesson from './interfaces/ILesson'
+import LessonService from './API/mainAPI';
+import { Slider, Switch } from 'antd';
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
 
 function App() {
+
+  const [lessons, setLessons] = useState<any[]>([]);
+  const [numRecords, setNumRecords] = useState(5);
+  const [rangeNumEmo, setrangeNumEmo] = useState([1,3]);
+
+
+  const columns: ColumnsType<any> = [
+    {
+      title: 'Текст',
+      dataIndex: '_id',
+      key: '_id',
+    },
+    {
+      title: 'Эмоция',
+      dataIndex: 'emotions',
+      key: 'emotions',
+    },];
+
+
+
+
+  useEffect(() => {
+    fetchLessons();
+  }, [numRecords, rangeNumEmo]);
+
+  async function fetchLessons() {
+    try {
+      const res = await LessonService.getSomeRecordsWithVaryEmo(rangeNumEmo[0], rangeNumEmo[1], numRecords);
+      setLessons(res.data);
+      // console.log(lessons)
+    }
+    catch(e) {
+      console.error('Error: ', e);
+    }
+  }
+
+  const [disabled, setDisabled] = useState(false);
+
+  const onChange = (newValue: number) => {
+    setNumRecords(newValue);
+  };
+
+  const onRangeChange = (newValue: any) => {
+    console.log(newValue)
+    setrangeNumEmo(newValue);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Slider defaultValue={30} value={numRecords} onChange={onChange} disabled={disabled} />
+      <Slider range value={rangeNumEmo} onChange={onRangeChange} defaultValue={[1, 10]} disabled={disabled} />
+      <Table columns={columns} dataSource={lessons} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/interfaces/ILesson.ts
@@ -0,0 +1,11 @@
+interface ILesson {
+    id: number;
+    course: string;
+    type: string;
+    text: String;
+    image: string;
+    updatedAt: string;
+    createdAt: string;
+  }
+  
+  export default ILesson;
